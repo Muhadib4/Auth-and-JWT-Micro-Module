7f3c74abc1f9cc0206c9d00719a24185fc5c4bc8
Muhadib4 <muhadibdwi@gmail.com>
2026-09-02 00:43:42 +0700

    feat: add JSON parsing and echo endpoint to server

--- src/server.js
+++ src/server.js
@@ -3,9 +3,18 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Auth & JWT Micro-Module API is running"
+  });
+});
+
+app.post("/echo", (req, res) => {
+  res.status(200).json({
+    message: "JSON berhasil diterima",
+    receivedData: req.body
   });
 });
 
